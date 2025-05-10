62a2b9856d90ae8099c875a9f96955e88e1910fe
eigger <eigger87@gmail.com>
2025-05-10 10:10:10 +0900

    Update devices.py

--- custom_components/gicisky/gicisky_ble/devices.py
+++ custom_components/gicisky/gicisky_ble/devices.py
@@ -11,56 +11,26 @@ class DeviceEntry:
     max_voltage: float = 2.9
     min_voltage: float = 2.2
 
-
-
-    # 0x0B: "GICI_BLE_EPD_21_BWR",
-    # 0x28: "GICI_BLE_EPD_29_BW",
-    # 0x30: "GICI_BLE_EPD_29_BW",
-    # 0x2B: "GICI_BLE_EPD_29_BWR",
-    # 0x33: "GICI_BLE_EPD_29_BWR1",
-    # 0x48: "GICI_BLE_EPD_BW_42",
-    # 0x4B: "GICI_BLE_EPD_BWR_42",
-    # 0x40: "GICI_BLE_TFT_BW_42",
-    # 0x42: "GICI_BLE_TFT_BWR_42",
-    # 0x68: "GICI_BLE_EPD_BW_74",
-    # 0x6A: "GICI_BLE_EPD_BWR_74",
-    # 0xEB: "GICI_BLE_EPD_BWR_29_SILABS",
-
-
-
-    # 0x0B: (250, 128),  # GICI_BLE_EPD_21_BWR
-    # 0x28: (296, 128),  # GICI_BLE_EPD_29_BW  (2.9��ġ, 296 �� 128 �ȼ�)
-    # 0x30: (296, 128),  # GICI_BLE_EPD_29_BW
-    # 0x2B: (296, 128),  # GICI_BLE_EPD_29_BWR
-    # 0x33: (296, 128),  # GICI_BLE_EPD_29_BWR1
-    # 0x48: (400, 300),  # GICI_BLE_EPD_BW_42  (4.2��ġ, 400 �� 300 �ȼ�)
-    # 0x4B: (400, 300),  # GICI_BLE_EPD_BWR_42
-    # 0x40: (400, 300),  # GICI_BLE_TFT_BW_42
-    # 0x42: (400, 300),  # GICI_BLE_TFT_BWR_42
-    # 0x68: (800, 480),  # GICI_BLE_EPD_BW_74  (7.4��ġ, 800 �� 480 �ȼ�)
-    # 0x6A: (800, 480),  # GICI_BLE_EPD_BWR_74
-    # 0xEB: (296, 128),  # GICI_BLE_EPD_BWR_29_SILABS (2.9��ġ)
-
 DEVICE_TYPES: dict[int, DeviceEntry] = {
     0xA0: DeviceEntry(
         name="TFT 21",
-        model="TFT 21 BW",
+        model="TFT 2.1\" BW",
         resolution=(250, 132),
         red=False
     ),
     0x0B: DeviceEntry(
         name="EPD 21",
-        model="EPD 21 BWR",
+        model="EPD 2.1\" BWR",
         resolution=(250, 128),
     ),
     0x32: DeviceEntry(
         name="EPD 29",
-        model="EPD 29 BWR",
+        model="EPD 2.9\" BWR",
         resolution=(296, 128),
     ),
     0x4B: DeviceEntry(
         name="EPD 42",
-        model="EPD 42 BWR",
+        model="EPD 4.2\" BWR",
         resolution=(400, 300),
         max_voltage=3.0
     ),

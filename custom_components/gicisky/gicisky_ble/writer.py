12295861fa3401587c1a4075cc7e85f01c8654bd
eigger <eigger87@gmail.com>
2025-05-11 00:46:35 +0900

    Update writer.py

--- custom_components/gicisky/gicisky_ble/writer.py
+++ custom_components/gicisky/gicisky_ble/writer.py
@@ -176,8 +176,8 @@ class GiciskyClient:
                         break
                     part = int.from_bytes(data[2:6], "little")
                     count += 1
-                    if part != count:
-                        raise Exception(f"Count Error: {part} {count}")
+                    # if part != count:
+                    #     raise Exception(f"Count Error: {part} {count}")
                 else:
                     break
         except Exception as e:
@@ -238,7 +238,7 @@ class GiciskyClient:
         # 두 번째 색상 포함 여부에 따라 합치기
         combined = byte_data + byte_data_red if self.support_red else byte_data
 
-        _LOGGER.debug(f"Image width:{width}, height:{height}, Len:{len(combined)}")
+        _LOGGER.debug(f"Image width:{width}, height:{height}, Len:{len(combined)},{self.packet_size}")
         # bytearray로 변환하여 반환
         return list(bytearray(combined))
 

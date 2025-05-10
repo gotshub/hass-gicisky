1a246522b2d8fe8d1a6fe7724dc3114f5733617d
eigger <eigger87@gmail.com>
2025-05-10 11:18:53 +0900

    Update parser.py

--- custom_components/gicisky/gicisky_ble/parser.py
+++ custom_components/gicisky/gicisky_ble/parser.py
@@ -92,3 +92,4 @@ class GiciskyBluetoothDeviceData(BluetoothData):
         self.update_predefined_binary_sensor(
             BinarySensorDeviceClass.CONNECTIVITY, conn
         )
+        return self._finish_update()
